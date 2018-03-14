6194628a27c9c70b1feedc487d3cd38f0017b3bf
VincentXue <vincentxueios@gmail.com>
2018-03-15 07:14:07 +0800

    First Commit

---
+++ BigFootWow/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BigFootWow
+//
+//  Created by VincentXue on 15/03/2018.
+//  Copyright © 2018 VincentXue. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
